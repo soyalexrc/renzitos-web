ca48b9322ac677f0c809c4733ccdf68d62fbfe35
alexrodriguez <alexcarvajal2404@gmail.com>
2024-12-03 20:54:57 -0500

    :sparkles: cors third attempt

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/api/:path*",
+                source: "/src/app/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
